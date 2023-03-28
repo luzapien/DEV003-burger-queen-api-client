e6fc573122eb2b916d59d3ba22e3ef3aace15ecf
María Rodríguez <lu.rzapien.92@gmail.com>
2023-03-28 15:23:49 -0600

    http request using fetch

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent {
     'email': new FormControl('', [Validators.required, Validators.email]),
     'password': new FormControl('', Validators.required),
   })
-  
+
   faSpinner = faSpinner;
 
   constructor(private router: Router) {
@@ -29,13 +29,27 @@ export class LoginComponent {
   async onSubmitForm() {
     this.isFormLoading = true
     const { value } = this.formUser
-    console.log(value)
+    console.log('Value:', value)
     try {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(this.router.navigate(['home']))
-        }, 3000)
-      })
+      // await new Promise((resolve) => {
+      //   setTimeout(() => {
+      //     resolve(this.router.navigate(['home']))
+      //   }, 3000)
+      // })
+      const response = await fetch('http://localhost:8080/login',
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(value)
+        })
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const required = await response.json()
+      console.log(required)
+      this.router.navigate(['home'])
     } catch (error) {
       console.log(error)
     } finally {
