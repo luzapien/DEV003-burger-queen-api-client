518ed28f1f59db04cb50f7c4d09e49134de633ad
Angie Muñoz <aemunozpl@gmail.com>
2023-04-14 13:37:38 -0500

    count

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -19,9 +19,10 @@ export class BillComponent {
   removeProductsTicket(productId: number) {
     this.addProductService.removeProductsTicket(productId);
   }
+
   totalPrice() {
     const total = this.addProductService.products.reduce((pv, cv) => {
-      return pv = pv + cv.price
+      return pv = pv + cv.product.price * cv.qty
     }, 0)
     return total
   }
@@ -34,9 +35,21 @@ export class BillComponent {
     // this.requestService.createOrder(order, token).subscribe({
     //   next:(response) => {
     console.log(this.addProductService.products, this.date)
-
-    //   }
-    // })
-   
   }
-}
+  
+  increment(){
+    this.addProductService.products.map((element)=>element.qty++)
+  }
+  decrement(){
+    let productsNumber:any=0;
+    this.addProductService.products.forEach((element)=> {
+      if(element.qty-1 < 0){
+        // element.qty--
+        return productsNumber;
+      }
+    })
+    }
+    public deleteProduct():void {
+      this.addProductService.products = this.addProductService.products.filter((product) => product.qty > 0 )
+    }
+  }
