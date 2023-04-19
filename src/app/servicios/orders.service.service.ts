966e4d2769ff7f05068421532adf6e447d34c367
Angie Muñoz <aemunozpl@gmail.com>
2023-04-19 16:24:43 -0500

    We finished the third story

--- src/app/servicios/orders.service.service.ts
+++ src/app/servicios/orders.service.service.ts
@@ -3,20 +3,20 @@ import { AddProductService } from '../servicios/addproducts.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Order} from 'src/types';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersServiceService {
 
-  constructor(private http: HttpClient) { } // servicio que conecta con la API
+  constructor(
+    private http: HttpClient) {} // servicio que conecta con la API
+
 
   url:string = 'http://localhost:8080/orders';
-  
-
   // api_key = sessionStorage.setItem("token", "accesstoken");
   accessToken = localStorage.getItem('accessToken');
   // accessToken =  this.cookieService.get('accessToken');
-
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -24,12 +24,18 @@ export class OrdersServiceService {
     })
   };
 
-  //metodos 
+  //Metodos para comunciarse con la API.
   getOrderService(): Observable<Order[]> {
     return this.http.get<Order[]>(this.url, this.httpOptions);
   }
 
-  postOrderService(products:Order): Observable<Order> {
-    return this.http.post<Order>(this.url,products, this.httpOptions);
+  postOrderService(orders:Order): Observable<Order> {
+    return this.http.post<Order>(this.url,orders,this.httpOptions);
   }
+
+  updateOrderService(id: number, updateOrder: Order):Observable<Order> {
+    const urlPatch = `${this.url}/${id}`;   // PATCH api/heroes/42
+    return this.http.patch<Order>(urlPatch, updateOrder, this.httpOptions);
+  }
+
 }
